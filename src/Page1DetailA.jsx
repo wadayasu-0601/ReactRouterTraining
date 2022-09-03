e847ee327aa87f2a35b058e02d233d93567a34ca
wadayasu-0601 <64833067+wadayasu-0601@users.noreply.github.com>
2022-09-03 17:59:51 +0900

    done

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,13 @@
+import { useLocation, useHistory } from "react-router-dom";
+export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+  const history = useHistory();
+  const onClickGoBack = () => history.goBack();
+  return (
+    <div>
+      <h1>Page1DetailAです</h1>
+      <button ocClick={onClickGoBack}>戻る</button>
+    </div>
+  );
+};
--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
-  return <div>Page2</div>;
+  return (
+    <div>
+      <h1>Page2</h1>
+      <Link to="/page2/999">Url Parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
+    </div>
+  );
 };
